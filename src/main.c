a3e8f43b086efc9f96f9fe0e3a3950815418ed2b
David Wilson <david@daviwil.com>
2022-03-24 15:48:13 +0200

    Update project.msc to reflect `mesche build` improvements

--- src/main.c
+++ src/main.c
@@ -1,31 +1,30 @@
 #include <mesche.h>
 #include <stdio.h>
-
-#include "engine/src/renderer.h"
-#include "engine/src/texture.h"
-#include "engine/src/window.h"
+#include <substratic.h>
 
 int main(int argc, char **argv) {
   VM vm;
   mesche_vm_init(&vm, argc, argv);
 
+  // TODO: Resolve all paths relative to executable path
+
   // Set up load paths and load core modules
+#ifdef DEV_BUILD
   mesche_vm_load_path_add(&vm, "./deps/substratic/engine/modules");
+#else
+  mesche_vm_load_path_add(&vm, "./modules");
+#endif
   mesche_vm_register_core_modules(&vm);
 
-  // Initialize Substratic modules in Mesche
-  subst_window_module_init(&vm);
-  subst_texture_module_init(&vm);
-  subst_renderer_module_init(&vm);
+  // Initialize the Substratic library in Mesche
+  substratic_library_init(&vm);
 
   // Evaluate the init script
-  mesche_vm_load_file(&vm, "src/main.msc");
-
-  // Report the final memory allocation statistics
-  /* mesche_mem_report((MescheMemory *)&vm); */
-
-  // Write out a final newline for now
-  printf("\n");
+/* #ifdef DEV_BUILD */
+/*   mesche_vm_load_file(&vm, "../../modules/main.msc"); */
+/* #else */
+  mesche_vm_load_file(&vm, "./modules/main.msc");
+/* #endif */
 
   // Free the VM and exit
   mesche_vm_free(&vm);
