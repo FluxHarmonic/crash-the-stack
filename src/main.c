7140b51a674fe9013de3693277b3046121e5825e
David Wilson <david@daviwil.com>
2022-04-03 07:02:12 +0300

    main: Resolve module path relative to program directory

--- src/main.c
+++ src/main.c
@@ -6,27 +6,61 @@ int main(int argc, char **argv) {
   VM vm;
   mesche_vm_init(&vm, argc, argv);
 
-  // TODO: Resolve all paths relative to executable path
+  // Set up the compiler's load path relative to the program path
+  char cwd[500];
+  char *program_path = mesche_process_executable_path();
+  char *program_dir = mesche_fs_file_directory(strdup(program_path));
 
-  // Set up load paths and load core modules
+  // Add the main modules path
+  char *tmp_path = mesche_cstring_join(program_dir, strlen(program_dir),
 #ifdef DEV_BUILD
-  mesche_vm_load_path_add(&vm, "./deps/substratic/engine/modules");
+                                       "/../../modules", 14,
 #else
-  mesche_vm_load_path_add(&vm, "./modules");
+                                       "/modules", 8,
 #endif
+                                       NULL);
+
+  // Register the main module path
+  char *modules_path = mesche_fs_resolve_path(tmp_path);
+  printf("Module path: %s\n", modules_path);
+
+  free(tmp_path);
+  mesche_vm_load_path_add(&vm, modules_path);
+
+  // Resolve the main file path
+  tmp_path = mesche_cstring_join(modules_path, strlen(modules_path),
+                                 "/main.msc", 9, NULL);
+  char *main_file_path = mesche_fs_resolve_path(tmp_path);
+
+  // Add the Substratic module path for dev builds
+#ifdef DEV_BUILD
+  tmp_path =
+      mesche_cstring_join(program_dir, strlen(program_dir),
+                          "/../../deps/substratic/engine/modules", 37, NULL);
+
+  char *subst_modules_path = mesche_fs_resolve_path(tmp_path);
+  mesche_vm_load_path_add(&vm, subst_modules_path);
+  free(subst_modules_path);
+#endif
+
+  // Register core Mesche modules
   mesche_vm_register_core_modules(&vm);
 
   // Initialize the Substratic library in Mesche
   substratic_library_init(&vm);
 
   // Evaluate the init script
-/* #ifdef DEV_BUILD */
-/*   mesche_vm_load_file(&vm, "../../modules/main.msc"); */
-/* #else */
-  mesche_vm_load_file(&vm, "./modules/main.msc");
-/* #endif */
+  mesche_vm_load_file(&vm, main_file_path);
 
-  // Free the VM and exit
+  // Free the VM
   mesche_vm_free(&vm);
+
+  // Free temporary strings
+  free(tmp_path);
+  free(main_file_path);
+  free(modules_path);
+  free(program_dir);
+  free(program_path);
+
   return 0;
 }
